850c7fb7367a656cc439eb33d161cf5ed691a6ff
Jake Mills <jake.mills.05@gmail.com>
2020-10-08 18:03:51 -0400

    Created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :auditions
+      resources :roles
+      resources :gigs
+      resources :casting_directors
+      resources :actors
+    end
+  end
 end
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
