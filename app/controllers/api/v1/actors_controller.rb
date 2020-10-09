e0d96af6297bde95f521ad607885a2efe18cc53c
Jake Mills <jake.mills.05@gmail.com>
2020-10-09 18:56:53 -0400

    Able to create new actor via fetch post request

--- app/controllers/api/v1/actors_controller.rb
+++ app/controllers/api/v1/actors_controller.rb
@@ -1,2 +1,18 @@
 class Api::V1::ActorsController < ApplicationController
+	
+	def create
+    actor = Actor.create(actor_params)
+    if actor.valid?
+      render json: { actor: ActorSerializer.new(actor) }, status: :created
+    else
+      render json: { error: 'failed to create user' }, status: :not_acceptable
+    end
+  end
+
+	private
+	
+  def actor_params
+    params.require(:actor).permit(:first_name, :last_name, :gender, :image, :email, :password, :address, :city, :state, :zip)
+	end
+	
 end
--- app/serializers/actor_serializer.rb
+++ app/serializers/actor_serializer.rb
@@ -1,3 +1,3 @@
 class ActorSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :gender, :image, :email, :password_digest, :address, :city, :state, :zip
+  attributes :id, :first_name, :last_name, :gender, :image, :email, :address, :city, :state, :zip
 end
