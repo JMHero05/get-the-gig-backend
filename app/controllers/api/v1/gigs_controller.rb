d29f2a24923d8f6eab229a14d1400f53d5db7bd8
Jake Mills <jake.mills.05@gmail.com>
2020-10-14 20:41:18 -0400

    Added gig controller index, show, and create w/strong params

--- app/controllers/api/v1/gigs_controller.rb
+++ app/controllers/api/v1/gigs_controller.rb
@@ -1,2 +1,29 @@
 class Api::V1::GigsController < ApplicationController
+	skip_before_action :authorized
+require 'pry'
+
+	def index
+		gigs = Gig.all
+		render json: gigs
+	end
+	
+	def show
+		gig = Gig.find(params[:id])
+		render json: gig
+	end
+
+	def create
+		# binding.pry
+		# dig_gig = gig_params.dig
+		gig = Gig.create(gig_params)
+		render json: gig.to_json
+	end
+
+
+
+	private
+
+	def gig_params
+    params.require(:gig).permit(:casting_director_id, :title, :gig_type, :union, :producer, :director, :choreographer, :music_director, :opening_date, :closing_date, :gig_location, :pay_rate, :audition_date, :audition_location)
+	end
 end
