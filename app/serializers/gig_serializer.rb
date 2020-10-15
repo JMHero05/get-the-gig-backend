c4c9c965a13a6f83cdc44d35495f71e1818923f5
Jake Mills <jake.mills.05@gmail.com>
2020-10-15 11:12:15 -0400

    Added roles to gig serializer

--- app/serializers/gig_serializer.rb
+++ app/serializers/gig_serializer.rb
@@ -1,4 +1,5 @@
 class GigSerializer < ActiveModel::Serializer
   attributes :id, :title, :gig_type, :union, :producer, :director, :choreographer, :music_director, :opening_date, :closing_date, :gig_location, :pay_rate, :audition_date, :audition_location
   has_one :casting_director
+  has_many :roles
 end
