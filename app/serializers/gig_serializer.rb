fa7a0cb78a5d0795fd1a0fad0360d0dac5d6f50c
Jake Mills <jake.mills.05@gmail.com>
2020-10-14 20:40:39 -0400

    Adjusted serializer

--- app/serializers/gig_serializer.rb
+++ app/serializers/gig_serializer.rb
@@ -1,4 +1,4 @@
 class GigSerializer < ActiveModel::Serializer
-  attributes :id, :title, :type, :union, :producer, :director, :choreographer, :music_director, :opening_date, :closing_date, :gig_location, :pay_rate, :audition_date, :audition_location, :roles
+  attributes :id, :title, :gig_type, :union, :producer, :director, :choreographer, :music_director, :opening_date, :closing_date, :gig_location, :pay_rate, :audition_date, :audition_location
   has_one :casting_director
 end
