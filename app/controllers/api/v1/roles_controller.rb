82ede4f7f1a41737cbfa82947b02614e5ed0b2c3
Jake Mills <jake.mills.05@gmail.com>
2020-10-16 15:50:59 -0400

    Added create and strong params to roles controller, modified casting route since it was looking for id the way it was before

--- app/controllers/api/v1/roles_controller.rb
+++ app/controllers/api/v1/roles_controller.rb
@@ -1,2 +1,15 @@
 class Api::V1::RolesController < ApplicationController
+	skip_before_action :authorized
+
+	def create
+		role = Role.create(role_params)
+		render json: role
+	end
+
+	private
+
+	def role_params
+		params.require(:role).permit(:gig_id, :role_type, :name, :description, :gender, :beg_age_range, :end_age_range)
+	end
+
 end
