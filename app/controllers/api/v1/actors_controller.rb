b98bbcd88dd49f63dac3d318ffbabf040bca80bf
Jake Mills <jake.mills.05@gmail.com>
2020-11-16 15:14:00 -0500

    Created show & audition methods in actor controller

--- app/controllers/api/v1/actors_controller.rb
+++ app/controllers/api/v1/actors_controller.rb
@@ -1,5 +1,12 @@
 class Api::V1::ActorsController < ApplicationController
-  skip_before_action :authorized, only: [:create]
+  skip_before_action :authorized, only: [:create, :show, :auditions]
+
+  require 'pry'
+
+  def show
+		actor = Actor.find(params[:id])
+		render json: actor
+	end
 	
 	def create
     @actor = Actor.create(actor_params)
@@ -9,6 +16,12 @@ class Api::V1::ActorsController < ApplicationController
     else
       render json: { error: 'failed to create user' }, status: :not_acceptable
     end
+  end
+
+  def auditions
+    actor = Actor.find(params[:id])
+    auditions = Audition.where("actor_id = #{actor.id}")
+    render json: auditions
   end
 
 	private
