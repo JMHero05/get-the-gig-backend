23aa5c90747209c4d35ab724817aec54244a649c
Jake Mills <jake.mills.05@gmail.com>
2020-11-16 15:15:04 -0500

    Created audition controller that allows for auditions to be created, updated, and find actor's auditions

--- app/controllers/api/v1/auditions_controller.rb
+++ app/controllers/api/v1/auditions_controller.rb
@@ -1,2 +1,27 @@
 class Api::V1::AuditionsController < ApplicationController
+	skip_before_action :authorized
+	require 'pry'
+	def create
+		# binding.pry
+		audition = Audition.create(audition_params)
+		render json: audition
+	end
+
+	def update
+		# binding.pry
+		audition = Audition.find(params[:id])
+		audition.update(audition_params)
+		render json: audition
+	end
+
+	def actor_auditions
+		auditions = Audition.where(params[:actor_id])
+		render json: auditions
+	end
+
+	private
+
+	def audition_params
+		params.require(:audition).permit(:actor_id, :role_id, :time, :location, :date, :requested, :confirmed, :rejected)
+	end
 end
