5fd2dd1e64e7d59671fedb4a0416179eca6c42a1
Jake Mills <jake.mills.05@gmail.com>
2020-11-16 15:30:59 -0500

    Added proper routing and custom routing for auditions

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :auditions
+      resources :auditions, only: [:create, :show, :update]
+        # get 'auditions/actor/[:id]', to: 'auditions#actor_auditions'
       resources :roles
       resources :gigs
       resources :casting_directors, only: [:create, :show]
         post 'casting_directors/login', to: 'auth#create_casting_director'
+        get "casting_directors/auditions/:id", to: 'casting_directors#auditions'
         get 'users/profile', to: 'casting_directors#profile'
-      resources :actors, only: [:create]
+      resources :actors, only: [:create, :show]
         post 'actors/login', to: 'auth#create_actor'
+        get "actors/auditions/:id", to: 'actors#auditions'
         get 'actors/profile', to: 'actors#profile'
     end
   end
